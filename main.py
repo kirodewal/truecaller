5a351cfe1ed82c99cc7ed49b645acfd965ebf3dd
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-22 20:14:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def about(client, message):
     client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,
                         text=f"`Update Channel` : [HxBots](t.me/HxBots)\n`Creator :` [Kamlesh Kirodewal](t.me/Kirodewal)\n`Language :` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku Professional](https://herokuapp.com/)",
                         disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Suppory Group", url="t.me/HxSupport")]]))
+            [[InlineKeyboardButton("Support Group", url="t.me/HxSupport")]]))
 
 
 @app.on_message(filters.command(["botlist"]))
