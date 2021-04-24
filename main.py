246647ab0617debdb84066de424fdf8564af1e77
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-24 21:40:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,14 +49,15 @@ def stats(client, message):
 def start(client, message):
     check_status = check(message.chat.id)
     client.send_message(chat_id=message.chat.id,
-                        text=f"`Hi` **{message.from_user.first_name}**\n Enter the number to search... \n Join Our Channel For Updates @HxBots",reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("About", callback_data="about"),
-             InlineKeyboardButton("Source", callback_data="src")]],
+                        text=f"`Hi` **{message.from_user.first_name}**\n Enter the number to search... \n Join Our Channel For Updates @HxBots",
+    reply_markup=InlineKeyboardMarkup(
+           [
+               [InlineKeyboardButton("About", callback_data="about")],
+               [InlineKeyboardButton("Source", callback_data="src")]
+           ],
            [
                [InlineKeyboardButton("Buy Me A Coffee ☕", url="https://pay2me.vercel.app/kkirodewal@okaxis")]
-           ]
-       )
-     )
+           ]))
 
 @app.on_message(filters.text)
 def echo(client, message):
