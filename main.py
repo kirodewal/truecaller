da4d02765ebc6338c902e0e9a0788089d9e41f43
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-24 20:03:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -177,5 +177,5 @@ def start(client, message):
             [[InlineKeyboardButton("About", callback_data="about"),
              InlineKeyboardButton("Source", callback_data="src"),
              InlineKeyboardButton("Buy Me A Coffee", url="https://pay2me.vercel.app/kkirodewal@okaxis")]]
-
-app.run()
+       )
+ app.run()
