984b9fe59c3e5f67788b7d2d04f3cccd688a64b7
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-24 20:18:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def newbt(client,callback_query):
 @app.on_message(filters.command(["about"]))
 def about(client, message):
     client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,
-                        text=f"`Update Channel` : [HxBots](t.me/HxBots)\n`Creator :` [Kamlesh Kirodewal](t.me/Kirodewal)\n`Language :` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku Professional](https://herokuapp.com/)",
+                        text=f"`**⭕Update Channel⭕` : @HxBots\n`⭕Creator⭕ :` @Kirodewal\n`⭕Language⭕ :` [Python3](https://python.org)\n`⭕Library⭕ :` [Pyrogram](https://docs.pyrogram.org/) \n`⭕Server⭕ :` [Heroku Professional](https://herokuapp.com/)**",
                         disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Support Group", url="t.me/HxSupport")]]))
 
